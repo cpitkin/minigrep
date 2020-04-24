f9e02b562ceca6b191660351fd954afb6bb9fe35
Charlie Pitkin <charlie.pitkin@gmail.com>
2020-04-24 12:11:39 -0400

    Updated the README, cleaned up config parsing
    Moved parse_config to new for reusability
    Added Rust Book chapter link in the README
    
    Signed-off-by: Charlie Pitkin <charlie.pitkin@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 minigrep
+
+This project is from the Rust Language Book, [Chapter 12](https://doc.rust-lang.org/book/ch12-00-an-io-project.html).
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::fs;
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = parse_config(&args);
+  let config = Config::new(&args);
 
   println!("Searching for: {}", config.query);
   println!("In the file: {}", config.filename);
 
   let contents = fs::read_to_string(config.filename);
     match contents {
-      Ok(contents) => println!("{:?}", contents),
+      Ok(contents) => println!("{}", contents),
       Err(error) => println!("{:?}", error)
     }
 }
@@ -21,9 +21,11 @@ struct Config {
   filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-  let query = args[1].clone();
-  let filename = args[2].clone();
-
-  Config {query, filename}
+impl Config {
+  fn new(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+  
+    Config {query, filename}
+  }
 }
